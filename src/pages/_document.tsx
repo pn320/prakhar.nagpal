8a1495dd220ab6d230aa2796e580ada22b894782
Prakhar Nagpal <pn320@ic.ac.uk>
2022-06-26 18:51:53 +0100

    feat: change landing page content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,18 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import { ColorModeScript } from '@chakra-ui/react';
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+        </Head>
         <body>
-          {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
-import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import { Box } from "@chakra-ui/react";
+
 
 const Index = () => (
-  <DarkModeSwitch />
+  <Box px={"4"} py={'4'}>
+    Hey, there!  My name is Prakhar Nagpal. <br /> I'm a second year computing student at Imperial College London, currently working at FiveAI as a software engineering intern for the summer!
+  </Box>
 );
 
 export default Index
